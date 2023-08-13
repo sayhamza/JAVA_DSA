19d68b36b789e90fabb17cf272489c8ee74195ef
sayhamza <sayedhamza88882651@gmail.com>
2023-08-14 01:14:58 +0530

    Stack commit

---
+++ Stack/StackAL.java
@@ -0,0 +1,50 @@
+package Stack;
+
+import java.util.ArrayList;
+
+public class StackAL{
+    
+
+    static class Stack{
+        
+        static ArrayList<Integer> list=new ArrayList<>();
+        public static boolean isEmpty(){
+            return list.size()==0;
+        }
+        //PUSH
+        public static void push(int data){
+            list.add(data);
+        }
+        //POP
+        public static int pop(){
+            if (isEmpty()) {
+                return -1;   
+            }
+            int top=list.get(list.size()-1);
+            list.remove(list.size()-1);
+            return top;
+        }
+        //PEEK
+        public static int peek(){
+              if (isEmpty()) {
+                return -1;   
+            }
+            return list.get(list.size()-1);
+        }
+        }
+
+    public static void main(String[] args) {
+        // Stack s =new Stack();
+        // s.push(1);
+        // s.push(2);
+        // s.push(3);
+
+        // while (!s.isEmpty()) {
+        //     System.out.println(s.peek());
+        //     s.pop();
+            
+        // }
+
+
+    }
+}
---
+++ Stack/StockSpan.java
@@ -0,0 +1,11 @@
+package Stack;
+
+public class StockSpan {
+
+    
+        
+    public static void main(String[] args) {
+        
+    }
+    
+}
