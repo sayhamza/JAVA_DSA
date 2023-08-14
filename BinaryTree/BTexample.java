021cce8a3aa47f92deabc905ff29bd73d460cc2a
sayhamza <sayedhamza88882651@gmail.com>
2023-08-15 00:21:27 +0530

    GreedyAlgo

---
+++ BinaryTree/BTexample.java
@@ -0,0 +1,44 @@
+package BinaryTree;
+
+public class BTexample {
+    static class Node{
+        int data;
+        Node left;
+        Node right;
+        
+        Node (int data){
+            this.data=data;
+            this.left=null;
+            this.right=null;
+        }
+    }
+
+    static class binaryTree{
+        static int idx=-1;
+        public static Node buildTree(int nodes[]){
+            idx++;
+            if (nodes[idx]==-1) {
+                return null;  
+            }
+            Node newNode=new Node(nodes[idx]);
+            newNode.left=buildTree(nodes);
+            newNode.right=buildTree(nodes);
+
+            return newNode;
+        }
+
+        //Preorder Traversal root>leftSubtree>rightSubtree
+
+        public static void preorder(Node root){
+            
+        }
+    }
+
+    public static void main(String[] args) {
+        // int nodes[]={1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        // binaryTree tree=new binaryTree();
+        // Node root=tree.buildTree(nodes);
+        // System.out.println(root.data);
+    }
+    
+}
--- GreedyAlgo/ActivitySelection.java
+++ GreedyAlgo/ActivitySelection.java
@@ -14,7 +14,7 @@ public class ActivitySelection {
     public static void main(String[] args) {
         int start[]={1,3,0,5,8,5};
         int end[]={2,4,6,7,9,9};
-
+ 
         //Sorting 
         int activities[][]=new int[start.length][3];
         for (int i = 0; i < start.length; i++) {
