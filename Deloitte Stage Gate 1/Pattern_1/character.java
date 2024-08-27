5e3ee9febaf952ec560b543abbe6815ab723606d
Hamza Sayed <say.hamza@outlook.com>
2024-08-27 17:29:33 +0530

    pattern folder added

---
+++ Deloitte Stage Gate 1/Pattern_1/character.java
@@ -0,0 +1,17 @@
+package Pattern_1;
+
+public class character {
+    public static void main(String[] args) {
+        int n=4;
+        char ch='A';
+        //outer loop
+        for (int line = 0; line <=n; line++) {
+            //inner loop
+            for (int chars = 0; chars <=line; chars++) {
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
